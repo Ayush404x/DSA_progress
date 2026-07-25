67c425a809a40532cb1a3324dc1d48e3a32d075a
Ayush Kumar <ayushmuz88@gmail.com>
2026-07-26 01:10:47 +0530

    2d arrays

---
+++ 7_2d_array/02_traversal_of2d_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    //    for(int i=0;i<=2;i++){
+    //     cout<<arr[0][i]<<" ";
+    //    }
+    for (int i = 0; i <= 2; i++) // i is for rows
+    {
+        for (int j = 0; j <= 2; j++) // j is for columns
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
