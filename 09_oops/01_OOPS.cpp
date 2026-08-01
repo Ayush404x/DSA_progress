e87ac820e87c6df58176db7698a6435cf951f327
Ayush Kumar <ayushmuz88@gmail.com>
2026-08-02 02:11:35 +0530

    OOPS

--- 09_oops/01_OOPS.cpp
+++ 09_oops/01_OOPS.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 class Teacher
 {
+    //Acccess modifiers of theree types 1.public 2.private 3.protected 
 private:
     // Private Property
     double salary;
 
 public:
     // Public Properties
+    Teacher(){
+        dept = "Computer science";
+    }
     string name;
     string dept;
     string subject;
@@ -44,19 +48,25 @@ public:
 int main()
 {
     // Creating Object
+    
+    //Constructor ko call jati jaissse hum loog object banate hai 
+    //here we have made only one object from teacher class 
+    //so thata why contructor is printed only once in this code
+    //if we create two object from the it will print constructor twice
     Teacher t1;
+    Teacher t2;
 
     t1.name = "Shradha";
-    t1.dept = "Computer Science";
+    //t1.dept = "Computer Science";
     t1.subject = "C++";
 
     // Setting private variable using setter
     t1.setSalary(2500000);
 
-    cout << "Teacher Name    : " << t1.name << endl;
-    cout << "Department      : " << t1.dept << endl;
-    cout << "Subject         : " << t1.subject << endl;
-    cout << "Salary          : " << t1.getSalary() << endl;
-
+    // cout << "Teacher Name    : " << t1.name << endl;
+    // cout << "Department      : " << t1.dept << endl;
+    // cout << "Subject         : " << t1.subject << endl;
+    // cout << "Salary          : " << t1.getSalary() << endl;
+    cout<<t1.dept<<endl;
     return 0;
 }
---
+++ 09_oops/02_Encapsulation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Account{
+    private://datat hide by making it private
+    double balance;
+    string password;
+
+    public:
+    string accID;
+    string username;
+    
+};
+
+
+int main(){
+
+
+}
