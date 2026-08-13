b733cc20ddc1e35ee20f99cf4359d6650ff036a2
Ayush Kumar <ayushmuz88@gmail.com>
2026-08-14 01:34:03 +0530

    2D

--- 7_2d_array/multiplication_of_matrix.cpp
+++ 7_2d_array/multiplication_of_matrix.cpp
@@ -1,51 +1,77 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int m;
-    cout << "Enter the no rows \n";
-    cin >> m;
-    int n;
-    cout << "Enter the no columns \n";
-    cin >> n;
-    int p;
-    cout<<"enter the rows of second matrix";
-    cin>>p;
-    int q;
-    cout << "Enter the no columns \n";
-    cin>>q;
-    if(n==p)
-    {
-        int arr[m][n];
-        int arr[p][q];
-    for (int i = 0; i <= m - 1; i++) // i is for rows
-    {
-        for (int j = 0; j <= n - 1; j++) // j is for columns
-        {
-            cin >> arr[i][j];
-        }
-    }
-    for (int i = 0; i <= p - 1; i++) // i is for rows
-    {
-        for (int j = 0; j <= q - 1; j++) // j is for columns
-        {
-            cin >> arr[p][q];
-        }
-    }
-        int res[m][n];
-        for(int i =0;i<m;i++){
-            for(int j =0;i<n;j++)
-            {
-                //res of i and j
-                //res[i][j] = a[i][0]+b[0][j]+a[i][1]+a[i][1]*b[1][i]+
+    int m, n;
+    cout << "Enter the rows and columns of first matrix: ";
+    cin >> m >> n;
 
-                res[i][j];
+    int p, q;
+    cout << "Enter the rows and columns of second matrix: ";
+    cin >> p >> q;
+
+    // Matrix multiplication is possible only when
+    // columns of first matrix == rows of second matrix
+    if (n != p)
+    {
+        cout << "The matrices cannot be multiplied.";
+        return 0;
+    }
+
+    int a[m][n];
+    int b[p][q];
+
+    // Input first matrix
+    cout << "Enter elements of first matrix:\n";
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    // Input second matrix
+    cout << "Enter elements of second matrix:\n";
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+
+    // Result matrix will have m rows and q columns
+    int res[m][q];
+
+    // Matrix multiplication
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            res[i][j] = 0;
+
+            for (int k = 0; k < n; k++)
+            {
+                res[i][j] += a[i][k] * b[k][j];
             }
         }
-}
-    else{
-       cout<<"The matrix can not  be multiplies";
-
     }
-}
 
+    // Print result
+    cout << "Resultant matrix:\n";
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
