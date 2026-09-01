6df73a84b679324da22ff71f7f4c585f5db1d2c8
Ayush Kumar <ayushmuz88@gmail.com>
2026-09-02 02:31:30 +0530

    strings

---
+++ 11_strings/stoi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    string str ="123456";
+    int x = stoi(str);
+    cout<<x;
+    cout<<x+1;
+
+    //stoll - used to convert in long long data type
+    long long x = stoll(str);
+    cout<<x;
+    
+
+
+    // int a = 123456;
+    // string s = to_string(a); 
+
+}
